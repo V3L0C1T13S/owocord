d509f0b52c99fcbd5863dd465e3afe3a8a8124c5
V3L0C1T13S <turtletester20@gmail.com>
2022-07-13 12:42:05 -0400

    chore: correct formatting

--- index.ts
+++ index.ts
@@ -1,27 +1,28 @@
-import { log } from '@rikka/API/Utils';
-import { owoifyText } from '@rikka/API/Utils/strings/owoify';
-import RikkaPlugin from '@rikka/Common/entities/Plugin';
+import { log } from "@rikka/API/Utils";
+import { owoifyText } from "@rikka/API/Utils/strings/owoify";
+import RikkaPlugin from "@rikka/Common/entities/Plugin";
 
 export default class ExamplePlugin extends RikkaPlugin {
-    inject() {
-        log("owo");
+  inject() {
+    log("owo");
 
-        document.body.addEventListener("DOMNodeInserted", (event) => {
-            this.recurse(event.target);
-        });
-        this.recurse(document.body);
-    }
+    document.body.addEventListener("DOMNodeInserted", (event) => {
+      if (event.target instanceof HTMLElement) {
+        this.recurse(event.target);
+      }
+    });
+    this.recurse(document.body);
+  }
 
-    private recurse(node: any) {
-        if (
-            ["STYLE", "SCRIPT", "NOSCRIPT", "IFRAME", "OBJECT"].includes(node.tagName)
-        )
-            return;
-        for (let child of node.childNodes) {
-            this.recurse(child);
-        }
-        if (node.nodeType === 3 && node.nodeValue != null) {
-            node.nodeValue = owoifyText(node.nodeValue);
-        }
+  private recurse(node: HTMLElement) {
+    if (
+      ["STYLE", "SCRIPT", "NOSCRIPT", "IFRAME", "OBJECT"].includes(node.tagName)
+    ) { return; }
+    node.childNodes.forEach((child) => {
+      this.recurse(child as HTMLElement);
+    });
+    if (node.nodeType === 3 && node.nodeValue != null) {
+      node.nodeValue = owoifyText(node.nodeValue);
     }
+  }
 }
